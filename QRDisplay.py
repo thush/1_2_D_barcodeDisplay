8d1b4932faf4b26856f8048eb8dd6a700cb8816b
thusinthaka <rqvn43@zebra.com>
2021-05-10 17:42:06 +0530

    latestChanges with new .gif files

--- QRDisplay.py
+++ QRDisplay.py
@@ -5,19 +5,35 @@ import EinkDisplay
 
 def rgb2gray(rgb):
     return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
-# load the image
-# img = imread('80_80_QRCode.gif')
+
+# Read-In the image
+
+img = imread('296-128-qr_.bmp')
+# img = imread('296_128_Redone.gif')
 # img = imread('100_100_DATAMATRIX_barcode.png')
-# img = imread('xx1.gif')
-img = imread('barcode.gif')
-# img = imread('100_100_DATAMATRIX_barcode.png')
-# img = imread('BZebra.gif')
+# img = imread('296_128_USPostal.bmp')
+# img = imread('296_128_UKPostal.bmp')
+
 
 gray = rgb2gray(img)
+
+img1 = np.asarray(gray, dtype=np.uint8)
+h = gray.shape[0]
+w = gray.shape[1]
+ww = 0
+if (w % 8):
+	ww = w // 8 + 1
+else:
+	ww = w // 8
+# print('ww -> ', ww)
+# print('size', h, w)
+# print('New Re-sized ', img1.size)
+
+
 data = np.zeros((4736,), dtype=np.uint8)
 i = 0
 j = 0
-A = np.zeros((100, 100))  # [gray.shape[0]][gray.shape[0]]
+A = np.zeros((128, 296))  # [gray.shape[0]][gray.shape[0]]
 t = 7
 k = 0
 temp = 0
@@ -27,24 +43,21 @@ for row in gray:
             A[i][j] = 0
         else:
             A[i][j] = 255
-            print('<',hex(temp), end='--')
+            # print('<',hex(temp), end='--')
             temp = temp | (1 << t)
             # print(hex(temp),'>')
-        # print(A[i][j],t, end=' ')
         if (t > 0):
             t = t - 1
         else:
             data[k] = temp
-            # print('<',format(temp, '02x'),' ',format(data[k], '02x'),'--')
             t = 7
             temp = 0
             k = k + 1
 
         # print("cell" ,i, j, end=' ')
         j = j + 1
-    # print('')
-    # print('<-------------------->')
     i = i + 1
     j = 0
+
 B = A.transpose()
-EinkDisplay.Eink_show(A)
+EinkDisplay.Eink_show(B)
